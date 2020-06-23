4a891f7ac42335567529f35e5e4a54019080ee2a
Mayank_MP5 <mayank5pande@gmail.com>
2020-06-23 18:11:41 +0530

    Ionic React Boilerplate Setup

--- src/App.tsx
+++ src/App.tsx
@@ -39,21 +39,21 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route path="/tab1" component={Tab1} exact={true} />
-          <Route path="/tab2" component={Tab2} exact={true} />
-          <Route path="/tab3" component={Tab3} />
-          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
+          <Route path="/stopwatch" component={Tab1} exact={true} />
+          <Route path="/clock" component={Tab2} exact={true} />
+          <Route path="/timer" component={Tab3} />
+          <Route path="/" render={() => <Redirect to="/stopwatch" />} exact={true} />
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
+          <IonTabButton tab="stopwatch" href="/stopwatch">
             <IonIcon icon={triangle} />
             <IonLabel>Tab 1</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
+          <IonTabButton tab="clock" href="/clock">
             <IonIcon icon={ellipse} />
             <IonLabel>Tab 2</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
+          <IonTabButton tab="timer" href="/timer">
             <IonIcon icon={square} />
             <IonLabel>Tab 3</IonLabel>
           </IonTabButton>
